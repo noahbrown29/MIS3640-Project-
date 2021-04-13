6a034dde6007cb057bb33f8fcb1b4743226a3e14
Noah Brown <nbrown9@babson.edu>
2021-04-13 16:37:33 -0400

    updated files

---
+++ main/main.py
@@ -0,0 +1,5 @@
+from exchangerates import exchangerates
+from weather import weather
+exchangerates()
+weather()
+
--- main/translator.py
+++ main/translator.py
@@ -8,11 +8,11 @@ pip install googletrans==3.1.0a0
 
 import googletrans
 from googletrans import Translator
-
-translator = Translator()
-translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='mi')
-for translation in translations:
-    print(translation.origin, ' -> ', translation.text)
+def translator():
+    translator = Translator()
+    translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='mi')
+    for translation in translations:
+        print(translation.origin, ' -> ', translation.text)
 # The quick brown fox  ->  빠른 갈색 여우
 # jumps over  ->  이상 점프
 # the lazy dog  ->  게으른 개
