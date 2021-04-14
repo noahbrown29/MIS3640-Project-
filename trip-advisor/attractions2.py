6adb5008cc47ce948d17fdb40fae31a9d88480a5
JuliaRealander <jrealander1@babson.edu>
2021-04-14 18:26:07 -0400

    node-connection-error

--- trip-advisor/attractions2.py
+++ trip-advisor/attractions2.py
@@ -3,20 +3,15 @@
 # 2. Is there a way to always have "TripAdvisor" within the search part of the url (q=) but still allow the user to input the rest of their query?
 
 """
-Make sure chromedriver is installed (Below is the correct link for windows computers using Chrome version 89)
-https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_win32.zip
+Make sure chromedriver is installed (Below is the correct link for windows computers using Chrome version 90)
+https://chromedriver.storage.googleapis.com/index.html?path=90.0.4430.24/
 """
   
-# from selenium import webdriver
-# import time
-# from selenium.webdriver.common.keys import Keys
-# import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re,datetime,os,sys
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -39,3 +34,10 @@ for i in range(1):
 
 
 driver.find_elements_by_tag_name('h3').send_keys("\n")
+# time.sleep(3)
+# results = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')
+# results[0].click()
+
+# driver.implicitly_wait(5)
+
+# driver.find_element_by_xpath('//a[starts-with(@href,"http://www.tripadvisor.com")]').click()
